d16230270e22d0388914a7802244c364421b28ca
Azreaal <contact@propernouns.xyz>
2022-07-02 23:28:33 -0700

    Updating site

--- packages/nouns-webapp/src/components/Documentation/index.tsx
+++ packages/nouns-webapp/src/components/Documentation/index.tsx
@@ -6,6 +6,13 @@ import Link from '../Link';
 import { Trans } from '@lingui/macro';
 
 const Documentation = () => {
+  const nounsLink = (
+    <Link
+      text={<Trans>Nouns DAO</Trans>}
+      url="https://www.nouns.wtf"
+      leavesPage={true}
+    />
+  );
   const cryptopunksLink = (
     <Link
       text={<Trans>Cryptopunks</Trans>}
@@ -39,15 +46,16 @@ const Documentation = () => {
           </h1>
           <p className={classes.aboutText}>
             <Trans>
-              Nouns are an experimental attempt to improve the formation of on-chain avatar
-              communities. While projects such as {cryptopunksLink} have attempted to bootstrap
+
+              Proper Nouns are an experimental attempt to improve the formation of on-chain avatar communities
+              by expanding upon the {nounsLink} framework. While projects such as {cryptopunksLink} have attempted to bootstrap
               digital community and identity, Nouns attempt to bootstrap identity, community,
               governance, and a treasury that can be used by the community.
             </Trans>
           </p>
           <p className={classes.aboutText} style={{ paddingBottom: '4rem' }}>
             <Trans>
-              Learn more below, or start creating Nouns off-chain using the {playgroundLink}.
+              Learn more below, or start creating Proper Nouns off-chain using the {playgroundLink}.
             </Trans>
           </p>
         </div>
@@ -59,40 +67,45 @@ const Documentation = () => {
             <Accordion.Body>
               <ul>
                 <li>
-                  <Trans>Nouns artwork is in the {publicDomainLink}.</Trans>
+                  <Trans>Proper Nouns artwork is in the {publicDomainLink}.</Trans>
                 </li>
                 <li>
-                  <Trans>One Noun is trustlessly auctioned every 24 hours, forever.</Trans>
+                  <Trans>One Proper Noun is trustlessly auctioned every hour, forever.</Trans>
                 </li>
                 <li>
-                  <Trans>100% of Noun auction proceeds are trustlessly sent to the treasury.</Trans>
+                  <Trans>100% of Proper Noun auction proceeds are trustlessly sent to the treasury.</Trans>
                 </li>
                 <li>
                   <Trans>Settlement of one auction kicks off the next.</Trans>
                 </li>
                 <li>
-                  <Trans>All Nouns are members of Nouns DAO.</Trans>
+                  <Trans>All Proper Nouns are members of Proper Nouns DAO.</Trans>
                 </li>
                 <li>
-                  <Trans>Nouns DAO uses a fork of {compoundGovLink}.</Trans>
+                  <Trans>Proper Nouns DAO uses a fork of {nounsLink}, itself a fork of {compoundGovLink}.</Trans>
                 </li>
                 <li>
-                  <Trans>One Noun is equal to one vote.</Trans>
+                  <Trans>One Proper Noun is equal to one vote.</Trans>
                 </li>
                 <li>
-                  <Trans>The treasury is controlled exclusively by Nouns via governance.</Trans>
+                  <Trans>The treasury is controlled exclusively by Proper Nouns via governance.</Trans>
                 </li>
                 <li>
                   <Trans>Artwork is generative and stored directly on-chain (not IPFS).</Trans>
                 </li>
                 <li>
                   <Trans>
-                    No explicit rules exist for attribute scarcity; all Nouns are equally rare.
+                    No explicit rules exist for attribute scarcity; all Proper Nouns are equally rare.
                   </Trans>
                 </li>
                 <li>
                   <Trans>
-                    Nounders receive rewards in the form of Nouns (10% of supply for first 5 years).
+                    ProNounders receive rewards in the form of Proper Nouns (9% of supply for first 5 years).
+                  </Trans>
+                </li>
+                <li>
+                  <Trans>
+                    GrandNounders receive rewards in the form of Proper Nouns (1% of supply for first 5 years).
                   </Trans>
                 </li>
               </ul>
@@ -101,28 +114,28 @@ const Documentation = () => {
 
           <Accordion.Item eventKey="1" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
-              <Trans>Daily Auctions</Trans>
+              <Trans>Hourly Auctions</Trans>
             </Accordion.Header>
             <Accordion.Body>
               <p className={classes.aboutText}>
                 <Trans>
-                  The Nouns Auction Contract will act as a self-sufficient Noun generation and
-                  distribution mechanism, auctioning one Noun every 24 hours, forever. 100% of
-                  auction proceeds (ETH) are automatically deposited in the Nouns DAO treasury,
-                  where they are governed by Noun owners.
+                  The Proper Nouns Auction Contract will act as a self-sufficient Proper Noun generation and
+                  distribution mechanism, auctioning one Proper Noun every hour, forever. 100% of
+                  auction proceeds (ETH) are automatically deposited in the Proper Nouns DAO treasury,
+                  where they are governed by Proper Noun owners.
                 </Trans>
               </p>
 
               <p className={classes.aboutText}>
                 <Trans>
                   Each time an auction is settled, the settlement transaction will also cause a new
-                  Noun to be minted and a new 24 hour auction to begin.{' '}
+                  Proper Noun to be minted and a new one-hour auction to begin.{' '}
                 </Trans>
               </p>
               <p>
                 <Trans>
                   While settlement is most heavily incentivized for the winning bidder, it can be
-                  triggered by anyone, allowing the system to trustlessly auction Nouns as long as
+                  triggered by anyone, allowing the system to trustlessly auction Proper Nouns as long as
                   Ethereum is operational and there are interested bidders.
                 </Trans>
               </p>
@@ -130,16 +143,16 @@ const Documentation = () => {
           </Accordion.Item>
           <Accordion.Item eventKey="2" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
-              <Trans>Nouns DAO</Trans>
+              <Trans>Proper Nouns DAO</Trans>
             </Accordion.Header>
             <Accordion.Body>
               <Trans>
-                Nouns DAO utilizes a fork of {compoundGovLink} and is the main governing body of the
-                Nouns ecosystem. The Nouns DAO treasury receives 100% of ETH proceeds from daily
-                Noun auctions. Each Noun is an irrevocable member of Nouns DAO and entitled to one
-                vote in all governance matters. Noun votes are non-transferable (if you sell your
-                Noun the vote goes with it) but delegatable, which means you can assign your vote to
-                someone else as long as you own your Noun.
+                Proper Nouns DAO utilizes a fork {nounsLink}, itself a fork of {compoundGovLink} and is the main governing body of the
+                Proper Nouns ecosystem. The Proper Nouns DAO treasury receives 100% of ETH proceeds from hourly
+                Proper Noun auctions. Each Proper Noun is an irrevocable member of Proper Nouns DAO and entitled to one
+                vote in all governance matters. Proper Noun votes are non-transferable (if you sell your
+                Proper Noun the vote goes with it) but delegatable, which means you can assign your vote to
+                someone else as long as you own your Proper Noun.
               </Trans>
             </Accordion.Body>
           </Accordion.Item>
@@ -150,17 +163,17 @@ const Documentation = () => {
             <Accordion.Body>
               <p>
                 <Trans>
-                  In addition to the precautions taken by Compound Governance, Nounders have given
+                  In addition to the precautions taken by Compound Governance, ProNounders have given
                   themselves a special veto right to ensure that no malicious proposals can be
-                  passed while the Noun supply is low. This veto right will only be used if an
+                  passed while the Proper Noun supply is low. This veto right will only be used if an
                   obviously harmful governance proposal has been passed, and is intended as a last
                   resort.
                 </Trans>
               </p>
               <p>
                 <Trans>
-                  Nounders will proveably revoke this veto right when they deem it safe to do so.
-                  This decision will be based on a healthy Noun distribution and a community that is
+                  ProNounders will proveably revoke this veto right when they deem it safe to do so.
+                  This decision will be based on a healthy Proper Noun distribution and a community that is
                   engaged in the governance process.
                 </Trans>
               </p>
@@ -168,14 +181,14 @@ const Documentation = () => {
           </Accordion.Item>
           <Accordion.Item eventKey="4" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
-              <Trans>Noun Traits</Trans>
+              <Trans>Proper Noun Traits</Trans>
             </Accordion.Header>
             <Accordion.Body>
               <p>
                 <Trans>
-                  Nouns are generated randomly based Ethereum block hashes. There are no 'if'
-                  statements or other rules governing Noun trait scarcity, which makes all Nouns
-                  equally rare. As of this writing, Nouns are made up of:
+                  Proper Nouns are generated randomly based Ethereum block hashes. There are no 'if'
+                  statements or other rules governing Proper Noun trait scarcity, which makes all Proper Nouns
+                  equally rare. As of this writing, Proper Nouns are made up of:
                 </Trans>
               </p>
               <ul>
@@ -192,11 +205,11 @@ const Documentation = () => {
                   <Trans>heads (234) </Trans>
                 </li>
                 <li>
-                  <Trans>glasses (21)</Trans>
+                  <Trans>monocles (21)</Trans>
                 </li>
               </ul>
               <Trans>
-                You can experiment with off-chain Noun generation at the {playgroundLink}.
+                You can experiment with off-chain Proper Noun generation at the {playgroundLink}.
               </Trans>
             </Accordion.Body>
           </Accordion.Item>
@@ -207,8 +220,8 @@ const Documentation = () => {
             <Accordion.Body>
               <p>
                 <Trans>
-                  Nouns are stored directly on Ethereum and do not utilize pointers to other
-                  networks such as IPFS. This is possible because Noun parts are compressed and
+                  Proper Nouns are stored directly on Ethereum and do not utilize pointers to other
+                  networks such as IPFS. This is possible because Proper Noun parts are compressed and
                   stored on-chain using a custom run-length encoding (RLE), which is a form of
                   lossless compression.
                 </Trans>
@@ -227,24 +240,24 @@ const Documentation = () => {
           </Accordion.Item>
           <Accordion.Item eventKey="6" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
-              <Trans>Noun Seeder Contract</Trans>
+              <Trans>Proper Noun Seeder Contract</Trans>
             </Accordion.Header>
             <Accordion.Body>
               <p>
                 <Trans>
-                  The Noun Seeder contract is used to determine Noun traits during the minting
+                  The Proper Noun Seeder contract is used to determine Proper Noun traits during the minting
                   process. The seeder contract can be replaced to allow for future trait generation
-                  algorithm upgrades. Additionally, it can be locked by the Nouns DAO to prevent any
-                  future updates. Currently, Noun traits are determined using pseudo-random number
+                  algorithm upgrades. Additionally, it can be locked by the Proper Nouns DAO to prevent any
+                  future updates. Currently, Proper Noun traits are determined using pseudo-random number
                   generation:
                 </Trans>
               </p>
-              <code>keccak256(abi.encodePacked(blockhash(block.number - 1), nounId))</code>
+              <code>keccak256(abi.encodePacked(blockhash(block.number - 1), properNounId))</code>
               <br />
               <br />
               <p>
                 <Trans>
-                  Trait generation is not truly random. Traits can be predicted when minting a Noun
+                  Trait generation is not truly random. Traits can be predicted when minting a Proper Noun
                   on the pending block.
                 </Trans>
               </p>
@@ -252,13 +265,28 @@ const Documentation = () => {
           </Accordion.Item>
           <Accordion.Item eventKey="7" className={classes.accordionItem}>
             <Accordion.Header className={classes.accordionHeader}>
-              <Trans>Nounder's Reward</Trans>
+              <Trans>ProNounder's Reward</Trans>
             </Accordion.Header>
             <Accordion.Body>
               <p>
                 <Trans>
-                  'Nounders' are the group of ten builders that initiated Nouns. Here are the
-                  Nounders:
+                  'ProNounders' are the three builders that initiated the Proper Nouns. Here are the
+                  ProNounders:
+                </Trans>
+              </p>
+              <ul>
+                <li>
+                  <Link
+                    text="@azreaal"
+                    url="https://twitter.com/azreaal"
+                    leavesPage={true}
+                  />
+                </li>
+              </ul>
+              <p>
+                <Trans>
+                  'GrandNounders' are the group of ten builders that initiated the original Nouns. Here are the
+                  GrandNounders:
                 </Trans>
               </p>
               <ul>
@@ -305,18 +333,24 @@ const Documentation = () => {
               </ul>
               <p>
                 <Trans>
-                  Because 100% of Noun auction proceeds are sent to Nouns DAO, Nounders have chosen
-                  to compensate themselves with Nouns. Every 10th Noun for the first 5 years of the
-                  project (Noun ids #0, #10, #20, #30 and so on) will be automatically sent to the
-                  Nounder's multisig to be vested and shared among the founding members of the
+                  Because 100% of Proper Noun auction proceeds are sent to Proper Nouns DAO, ProNounders have chosen
+                  to compensate themselves with Proper Nouns. Every 10th Noun for the first 5 years of the
+                  project (Proper Noun ids #0, #10, #20, #30 and so on) will be automatically sent to the
+                  ProNounder's multisig to be vested and shared among the founding members of the
                   project.
                 </Trans>
               </p>
               <p>
                 <Trans>
-                  Nounder distributions don't interfere with the cadence of 24 hour auctions. Nouns
-                  are sent directly to the Nounder's Multisig, and auctions continue on schedule
-                  with the next available Noun ID.
+                  However, because this project was inspired by and utilizes the framework created and made available by
+                  the .
+                </Trans>
+              </p>
+              <p>
+                <Trans>
+                  Pro- and GrandNounder distributions don't interfere with the cadence of hourly auctions. Proper Nouns
+                  are sent directly to the specified Multisig, and auctions continue on schedule
+                  with the next available Proper Noun ID.
                 </Trans>
               </p>
             </Accordion.Body>
